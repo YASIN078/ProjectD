ea167aca9aadf88cd7405b7e3c9e3cdff7d62358
EmirSarikus <47593884+EmirSarikus@users.noreply.github.com>
2020-05-22 12:50:39 +0200

    Face
    It kinda works but still buggy.

--- motion_detection 2.py
+++ motion_detection 2.py
@@ -3,7 +3,7 @@ import numpy as np
 #import win32api
 
 cap = cv2.VideoCapture('Vid.mp4')
-#face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#face_cascade = cv2.CascadeClassifier('haarcascade_profileface.xml')
 frame_width = int( cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 
 frame_height =int( cap.get( cv2.CAP_PROP_FRAME_HEIGHT))
@@ -24,6 +24,11 @@ while cap.isOpened():
         _, thresh = cv2.threshold(blur, 50, 255, cv2.THRESH_BINARY)
         dilated = cv2.dilate(thresh, None, iterations=2)
         contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+       
+        '''faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(cap, (x, y), (x+w, y+h), (255, 255, 0), 3)'''
         
         for contour in contours:
 
@@ -31,22 +36,17 @@ while cap.isOpened():
 
             if cv2.contourArea(contour) < 900:
                 continue
-            cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 3)
+            #cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 3)
+            cv2.rectangle(frame1,(40,200),(x+w,y+h),(255,0,255),3),
             cv2.putText(frame1, "".format(), (x, y), cv2.FONT_HERSHEY_SIMPLEX,
             1, (0, 0, 255), 3)
             cv2.putText(frame1, "".format(), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
                        1, (0, 0, 255), 3)
         #cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
-
-            #faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
-            #for (x, y, w, h) in faces:
-
-                #cv2.rectangle(cap, (x, y), (x+w, y+h), (255, 255, 0), 3)
-
         image = cv2.resize(frame1, (1280,720))
         out.write(image)
-        cv2.imshow("feed", cv2.flip(frame1, 0))
+        cv2.namedWindow("vid", cv2.WINDOW_AUTOSIZE) 
+        cv2.imshow("vid", cv2.flip(frame1, 0))
         frame1 = frame2
         ret, frame2 = cap.read()
 
