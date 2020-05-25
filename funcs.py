76881ef0040d69a7c8cff327d71deae8cf2dc974
Soufyan <MetaByte149@gmail.com>
2020-05-25 08:34:32 +0200

    Standard rectangle display + example now and prototype

--- funcs.py
+++ funcs.py
@@ -2,32 +2,31 @@ import math
 
 DISTANCE_THRESHOLD = 50
 
+
 def reduceContours(rects):
+    return rects
     res = []
     targets = []
 
     for r in rects:
-        targets.append( Target(r[0], r[1], r[2], r[3]) )
+        targets.append(Target(r[0], r[1], r[2], r[3]))
 
     # first remove all the boxes inside other boxes
-    length = len(targets)
-    for i in range(length):
-        for j in range (i + 1, len(targets) - 1):
+    for i in range(len(targets)):
+        for j in range(i + 1, len(targets) - 1):
 
-            if i == length:
+            if i >= len(targets) or j >= len(targets):
                 break
 
-            if( check_collision(targets[i], targets[j])):
+            if (check_collision(targets[i], targets[j])):
                 targets[i].Combine(targets[j])
                 targets.remove(targets[j])
-
-
 
     return targets
 
 
-def check_collision(t1,t2):
-    #If any of the sides from A are outside of B
+def check_collision(t1, t2):
+    # If any of the sides from A are outside of B
     if (t1.y + t1.h <= t2.y):
         return False
     if (t1.y >= t2.y + t2.h):
@@ -36,12 +35,11 @@ def check_collision(t1,t2):
         return False
     if (t1.x >= t2.x + t2.w):
         return False
-    #If none of the sides from A are outside B
+    # If none of the sides from A are outside B
     return True
 
 
 class Target:
-
     __slots__ = ["x", "y", "w", "h"]
 
     def __init__(self, _x, _y, _w, _h):
@@ -49,37 +47,38 @@ class Target:
         self.y = _y
         self.w = _w
         self.h = _h
-    
+
     def Combine(self, other):
         self.ExtendTarget(other.x, other.y)
         self.ExtendTarget(other.x + other.w, other.y + other.h)
 
     def ExtendTarget(self, x, y):
-        if(self.x > x):
+        if (self.x > x):
             self.x = x
-        if(self.x + self.w < x):
+        if (self.x + self.w < x):
             self.w = x - self.x
-        if(self.y > y):
+        if (self.y > y):
             self.y = y
-        if(self.y + self.h < y):
+        if (self.y + self.h < y):
             self.h = y - self.y
-    
+
     def GetMiddle(self):
-        X = self.x + self.w/2
-        Y = self.y + self.h/2
-        return (X,Y)
-    
+        X = self.x + self.w / 2
+        Y = self.y + self.h / 2
+        return (X, Y)
+
     def Distance(self, other):
         p1 = self.GetMiddle()
         p2 = other.GetMiddle()
         return dist(p1, p2)
-        
-        
+
+
 def dist(x1, y1, x2, y2):
-    return math.sqrt( ((x1-x2)**2)+((y1-y2)**2) )
+    return math.sqrt(((x1 - x2) ** 2) + ((y1 - y2) ** 2))
+
 
 def dist(p1, p2):
-    return math.sqrt( ((p1[0]-p2[0])**2)+((p1[1]-p2[1])**2) )
+    return math.sqrt(((p1[0] - p2[0]) ** 2) + ((p1[1] - p2[1]) ** 2))
 
 
 if __name__ == "__main__":
@@ -97,5 +96,5 @@ if __name__ == "__main__":
 
     for x in a:
         print(x)
-    
+
     print(len(test))
