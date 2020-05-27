c88bfd5b1e4822ed95bd53714d2f7242f750d646
Soufyan <MetaByte149@gmail.com>
2020-05-27 15:46:41 +0200

    Made real new version

--- image_recog.py
+++ image_recog.py
@@ -1,80 +1,59 @@
 import cv2
-import numpy as np
+import numpy
 import math
 import sys
+
 
 class Target:
 
     def __init__(self, _box, _frame):
-        self.box = _box
+        self.x = int(_box[0])
+        self.y = int(_box[1])
+        self.w = int(_box[2])
+        self.h = int(_box[3])
+
         self.tracker = cv2.TrackerCSRT_create()
-        self.tracker.init(frame, (_box[0], _box[1], _box[2], _box[3]))
+        self.tracker.init(_frame, (_box[0], _box[1], _box[2], _box[3]))
         self.items = []
 
     def update(self, frame):
-        _, self.box = self.tracker.update(frame)
-        cv2.rectangle(frame,
-        (
-            int(self.box[0]), int(self.box[1])
-        ),
+        # getting the new coordinates from box and converting to int
+        _, box = self.tracker.update(frame)
+        self.x = int(box[0])
+        self.y = int(box[1])
+        self.w = int(box[2])
+        self.h = int(box[3])
 
-          (
-              int(self.box[0] + self.box[2]),
-              int(self.box[1] + self.box[3]) )
-                      , (200, 0, 200), 5, 1)
+        # drawing the rectangle at new location
+        cv2.rectangle(frame, (self.x, self.y),
+                      (self.x + self.w, self.y + self.h), (200, 0, 200), 5, 1)
 
-
-
-
-vidcap = cv2.VideoCapture('Souf.m4v')
+        # drawing the text next to the rectangle in a column
+        y = 0
+        for y, item in enumerate(self.items):
+            cv2.putText(frame, item, (int(self.x) + int(self.w),
+                                      int(self.y) + 25 * y), cv2.QT_FONT_BLACK, 1, (0, 0, 0), 1)
+            
+vidcap = cv2.VideoCapture("Souf.m4v")
 success, frame = vidcap.read()
-
-# if len(sys.argv) > 0:
-#     # selection box
-#     print(cv2.selectROI("_", frame));cv2.destroyWindow("_")
-#     quit(0)
-
-# customer box tuple
-# box = (18, 333, 113, 199) # PROCESSING ANIMATION
-# box = (505, 106, 112, 118)
-targets = [Target((468, 98, 169, 194), frame), Target((350, 123, 78, 102), frame)]
-
-# print(frame[313, 236])
-
-# tracker = cv2.TrackerCSRT_create()
-# tracker.init(frame, box)
-
-# aquired = False
+targets = [Target((353, 123, 51, 22), frame), Target((509, 108, 104, 73), frame)]
 
 
+if len(sys.argv) > 1:
+    print("HELLO")
+    print(cv2.selectROI("_", frame));cv2.destroyWindow("_")
+    quit(0)
 
+while True:
 
-frameCount = 0
+    print(ColorDistance(frame[325, 114], [ 59, 72, 200]))
 
-while success:
-    # imgray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # _, imgray = cv2.threshold(imgray, 127, 100, cv2.THRESH_BINARY)
-    # reading new frame and updating boxes
     success, frame = vidcap.read()
-    for t in targets:
-        t.update(frame)
-    frameCount+=1
-
-    # p1 = (int(box[0]), int(box[1]))
-    # p2 = (int(box[0] + box[2]), int(box[1] + box[3]))
-
-    # cv2.rectangle(frame, p1, p2, (200, 0, 200), 5, 1)
-    cv2.imshow("cv2", frame)
-
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(1) == ord('q') or not success:
         break
 
+    for t in targets:
+        t.update(frame)
 
-
-
-
-
-
-
-
-
+    cv2.rectangle(frame, (114, 326), (130, 347), (200, 0, 0), 1, 0)
+    cv2.imshow("cv2", frame)
